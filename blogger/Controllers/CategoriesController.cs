ed7c8be11e3088a4b361e2d9f981fe26225ab408
Bijjam Vinay <v-bivinay@microsoft.com>
2024-06-21 18:24:12 +0530

    Add project files.

---
+++ blogger/Controllers/CategoriesController.cs
@@ -0,0 +1,20 @@
+﻿
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace blogger.Controllers
+{
+    public class CategoriesController:ControllerBase
+    {
+        private readonly CategoriesController _dbCategory;
+
+        public CategoriesController(CategoriesController dbCategory)
+        {
+            _dbCategory = dbCategory;
+        }
+    
+    }
+}
---
+++ blogger/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using blogger.Models.Domain;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace blogger.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
